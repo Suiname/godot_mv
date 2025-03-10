5332a0b2d9132b228f35eaa7a7d69782808a0331
Suiname <jtham@sbcglobal.net>
2025-03-09 21:12:19 -0500

    Added delta to movement functions

--- metroidvania/scipts/player.gd
+++ metroidvania/scipts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 const GRAVITY = 1000
-const SPEED = 300
-const JUMP_SPEED = -300
-const JUMP_HEIGHT = 100
+@export var speed: int = 1000
+@export var max_speed: int = 300
+@export var slow_down_speed: int = 1700
+
+
+@export var jump_speed: int = -300
+@export var jump_horizontal_speed: int = 1000
+@export var jump_max_speed: int = 300
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 enum State {
@@ -34,13 +39,14 @@ func player_idle(_delta: float) -> void:
 	if is_on_floor():
 		current_state = State.IDLE
 
-func player_run(_delta: float) -> void:
+func player_run(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
 
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x += direction * speed * delta
+		velocity.x = clamp(velocity.x, -max_speed, max_speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, slow_down_speed * delta)
 
 	if direction != 0 and is_on_floor():
 		current_state = State.RUN
@@ -48,12 +54,13 @@ func player_run(_delta: float) -> void:
 
 func player_jump(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_SPEED
+		velocity.y = jump_speed
 		current_state = State.JUMP
 
 	if !is_on_floor() and current_state == State.JUMP:
 		var direction = Input.get_axis("move_left", "move_right")
-		velocity.x += direction * JUMP_HEIGHT * delta
+		velocity.x += direction * jump_horizontal_speed * delta
+		velocity.x = clamp(velocity.x, -jump_max_speed, jump_max_speed)
 
 func player_animations():
 	if current_state == State.IDLE:
@@ -62,3 +69,7 @@ func player_animations():
 		animated_sprite_2d.play("run")
 	elif current_state == State.JUMP:
 		animated_sprite_2d.play("jump")
+
+func input_movement():
+	var direction = Input.get_axis("move_left", "move_right")
+	return direction
