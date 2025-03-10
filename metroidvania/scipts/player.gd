e7f71bba0604f891fca80db61182094b0ba7a39b
Suiname <jtham@sbcglobal.net>
2025-03-10 13:02:44 -0500

    added camera boundary and bullet animations'

--- metroidvania/scipts/player.gd
+++ metroidvania/scipts/player.gd
@@ -10,22 +10,29 @@ const GRAVITY = 1000
 @export var jump_max_speed: int = 300
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var bullet = preload("res://scenes/bullet.tscn")
+@onready var muzzle: Marker2D = $Muzzle
+
 enum State {
 	IDLE,
 	RUN,
 	JUMP,
+	SHOOT,
 }
 
 var current_state: State
-
+var muzzle_position
 func _ready():
 	current_state = State.IDLE
+	muzzle_position = muzzle.position
 
 func _physics_process(delta: float) -> void:
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
 	player_jump(delta)
+	player_muzzle_position()
+	player_shooting(delta)
 
 	move_and_slide()
 
@@ -62,13 +69,33 @@ func player_jump(delta: float) -> void:
 		velocity.x += direction * jump_horizontal_speed * delta
 		velocity.x = clamp(velocity.x, -jump_max_speed, jump_max_speed)
 
+func player_shooting(delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0 and Input.is_action_just_pressed("shoot"):
+		var bullet_instance: Node2D = bullet.instantiate() as Node2D
+		bullet_instance.set("direction", direction)
+		bullet_instance.set("flip_h", direction < 0)
+		bullet_instance.global_position = muzzle.global_position
+		get_parent().add_child(bullet_instance)
+		current_state = State.SHOOT
+
+func player_muzzle_position():
+	var direction = input_movement()
+	if direction > 0:
+		muzzle.position.x = muzzle_position.x
+	elif direction <0:
+		muzzle.position.x = -muzzle_position.x
+
+
 func player_animations():
 	if current_state == State.IDLE:
 		animated_sprite_2d.play("idle")
-	elif current_state == State.RUN:
+	elif current_state == State.RUN and animated_sprite_2d.animation != "run_and_shoot":
 		animated_sprite_2d.play("run")
 	elif current_state == State.JUMP:
 		animated_sprite_2d.play("jump")
+	elif current_state == State.SHOOT:
+		animated_sprite_2d.play("run_and_shoot")
 
 func input_movement():
 	var direction = Input.get_axis("move_left", "move_right")
