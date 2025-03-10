3057e571455e54dc596b0af8a1d79a9a51b2f98b
Suiname <jtham@sbcglobal.net>
2025-03-09 22:45:50 -0500

    level 1 layout

---
+++ metroidvania/scipts/game_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	RenderingServer.set_default_clear_color(Color(.62,.37,.95))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
