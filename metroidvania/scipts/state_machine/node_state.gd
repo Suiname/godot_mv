fc19176c3cc73600a027d2a05717fe74de29f62a
Suiname <jtham@sbcglobal.net>
2025-03-11 01:18:27 -0500

    Added enemy state machine with player detection

---
+++ metroidvania/scipts/state_machine/node_state.gd
@@ -0,0 +1,15 @@
+class_name NodeState
+extends Node
+
+func on_process(delta: float) -> void:
+	pass
+
+func on_physics_process(delta: float) -> void:
+	pass
+
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
